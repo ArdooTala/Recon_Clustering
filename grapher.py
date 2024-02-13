cb4faab7e56c93d6ffc457087e74996fcd622a30
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2024-02-13 13:06:56 +0100

    Add pygraphviz

--- grapher.py
+++ grapher.py
@@ -1,17 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pygraphviz as pgv
 from example_graph import con, dep
-
-
-subax1 = plt.subplot()
 
 
 def viz_g(g):
     nx.draw_circular(g, with_labels=True, font_weight='bold')
     plt.show()
-
-
-viz_g(dep)
 
 
 def get_con_dep_graph_from_dep(dep_graph):
@@ -41,15 +36,15 @@ def get_dep_graph_from_connections(graph, nodes):
     return sub_ass
 
 
-def collapse_nodes(ass_dep, ass):
+def collapse_nodes(ass_dep, ass_con):
     con_dep = get_con_dep_graph_from_dep(ass_dep)
     viz_g(con_dep)
 
-    new_ass = ass.copy()
+    new_ass_con = ass_con.copy()
     new_ass_dep = ass_dep.copy()
-    new_graph = con_dep.copy()
-    while not nx.is_directed_acyclic_graph(new_graph):
-        condensed = nx.condensation(new_graph)
+    new_con_dep = con_dep.copy()
+    while not nx.is_directed_acyclic_graph(new_con_dep):
+        condensed = nx.condensation(new_con_dep)
         for scc_node in list(nx.topological_sort(condensed)):
             scc = condensed.nodes[scc_node]['members']
             print(f"SCC >> {scc}")
@@ -60,37 +55,37 @@ def collapse_nodes(ass_dep, ass):
             trm_dep_con = con_dep.subgraph(scc)
             viz_g(trm_dep_con)
 
-            trm_con_graph = get_dep_graph_from_connections(new_ass, scc)
+            trm_con_graph = get_dep_graph_from_connections(new_ass_con, scc)
             clusters = list(nx.weakly_connected_components(trm_con_graph))
             print(f"Main Clusters: {clusters}")
             segmented_dep_graph = nx.union_all([new_ass_dep.subgraph(cluster) for cluster in clusters])
             trm_con_con = get_con_dep_graph_from_dep(segmented_dep_graph)
             viz_g(trm_con_con)
 
-            viz_g(new_graph)
+            viz_g(new_con_dep)
             for edge in trm_dep_con.edges:
                 if not trm_con_con.has_edge(*edge):
                     print(f"Removing dep Edge: {edge}")
-                    new_graph.remove_edge(*edge)
-            viz_g(new_graph)
+                    new_con_dep.remove_edge(*edge)
+            viz_g(new_con_dep)
 
             for cluster in clusters:
-                clstr_nodes = [n for n in cluster if new_graph.has_node(n)]
+                clstr_nodes = [n for n in cluster if new_con_dep.has_node(n)]
                 print(f"{cluster} > CON Nodes > {clstr_nodes}")
 
                 cluster_node = f"C[{';'.join(clstr_nodes)}]"
-                new_graph.add_node(cluster_node, PART=True)
+                new_con_dep.add_node(cluster_node, PART=True)
                 for node in clstr_nodes:
                     print(f"Contracting NODE {cluster_node} and {node}")
-                    new_graph = nx.contracted_nodes(new_graph, cluster_node, node, self_loops=False)
-                viz_g(new_graph)
+                    new_con_dep = nx.contracted_nodes(new_con_dep, cluster_node, node, self_loops=False)
+                viz_g(new_con_dep)
 
                 ass_cluster_node = f"AC[{';'.join([str(c) for c in cluster])}]"
-                new_ass.add_node(ass_cluster_node, PART=True)
+                new_ass_con.add_node(ass_cluster_node, PART=True)
                 for node in cluster:
                     print(f"Contracting NODE {ass_cluster_node} and {node}")
-                    new_ass = nx.contracted_nodes(new_ass, ass_cluster_node, node, self_loops=False)
-                viz_g(new_ass)
+                    new_ass_con = nx.contracted_nodes(new_ass_con, ass_cluster_node, node, self_loops=False)
+                viz_g(new_ass_con)
 
                 ass_cluster_node = f"AC[{';'.join([str(c) for c in cluster])}]"
                 new_ass_dep.add_node(ass_cluster_node, PART=True)
@@ -99,12 +94,19 @@ def collapse_nodes(ass_dep, ass):
                     new_ass_dep = nx.contracted_nodes(new_ass_dep, ass_cluster_node, node, self_loops=False)
                 viz_g(new_ass_dep)
 
-            viz_g(new_graph)
-            viz_g(new_ass)
+            export_graph(new_con_dep, "con_dep")
+            export_graph(new_ass_con, "ass_con")
 
-    return new_graph
+    return new_con_dep
 
+
+def export_graph(g, name):
+    nx.nx_agraph.to_agraph(g).draw(f"exports/{name}.pdf", prog="dot")
+
+
+subax1 = plt.subplot()
+export_graph(dep, "dep")
 
 final_con = collapse_nodes(dep, con)
 print("FIN")
-viz_g(final_con)
+export_graph(final_con, "res")
