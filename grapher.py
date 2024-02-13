596ff409a7280f343bfac5fa9f566fe6d403f3a3
Ardeshir Talaei <ardeshir.talaei@iaac.net>
2024-02-13 18:20:37 +0100

    Improve Export

--- grapher.py
+++ grapher.py
@@ -14,7 +14,7 @@ def get_con_dep_graph_from_dep(dep_graph):
     con_dep = dep_graph.copy()
     # print("#" * 100)
     for n, t in dep_graph.nodes.data("TYPE"):
-        if t != "PART":
+        if t == "CONN":
             # print(f"{n} is NOT Part: {dep_graph.nodes[n]}")
             continue
         # print(f"{n} is Part")
@@ -37,14 +37,14 @@ def get_dep_graph_from_connections(graph, nodes):
     return sub_ass
 
 
-def collapse_nodes(graph, nodes, cluster_node, save_dict=None, node_type="-"):
+def collapse_nodes(graph, nodes, cluster_node, save_dict=None):
     subg = graph.subgraph(nodes)
     cluster_level_data = subg.nodes.data('level', default=-1)
     cluster_level = max([lvl for _, lvl in cluster_level_data]) + 1
     print(f"\t >> {cluster_node} >> CLUSTER LEVEL IS > {cluster_level}")
 
     # cluster_node = f"C[{';'.join([str(c) for c in nodes])}]"
-    graph.add_node(cluster_node, TYPE="PART", level=cluster_level)
+    graph.add_node(cluster_node, TYPE="CLUS", level=cluster_level)
     for node in nodes:
         # print(f"Contracting NODE {cluster_node} and {node}")
         graph = nx.contracted_nodes(graph, cluster_node, node, self_loops=False)
@@ -55,7 +55,9 @@ def collapse_nodes(graph, nodes, cluster_node, save_dict=None, node_type="-"):
         clst_list.append(
             (
                 cluster_node,
-                [str(c) for c in subg.nodes]
+                [str(c) for c in subg.nodes if subg.nodes[c].get("TYPE", None) == "PART"],
+                [str(c) for c in subg.nodes if subg.nodes[c].get("TYPE", None) == "CONN"],
+                [str(c) for c in subg.nodes if subg.nodes[c].get("TYPE", None) == "CLUS"]
             )
         )
         save_dict[cluster_level] = clst_list
@@ -106,14 +108,10 @@ def process_graph(ass_dep, ass_con):
                 print(f"{cluster_name} > {cluster} > CON Nodes > {clstr_nodes}")
 
                 # Update ConnectionsDependencyGraph
-                new_con_dep = collapse_nodes(
-                    new_con_dep, clstr_nodes, cluster_name, save_dict=clusters_dict_conns, node_type="CONN"
-                )
+                new_con_dep = collapse_nodes(new_con_dep, clstr_nodes, cluster_name)
 
                 # Update AssemblyConnectionsGraph
-                new_ass_con = collapse_nodes(
-                    new_ass_con, cluster, cluster_name, save_dict=clusters_dict_parts, node_type="PART"
-                )
+                new_ass_con = collapse_nodes(new_ass_con, cluster, cluster_name, save_dict=clusters_dict)
 
                 # Update AssemblyDependencyGraph
                 new_ass_dep = collapse_nodes(new_ass_dep, cluster, cluster_name)
@@ -134,8 +132,7 @@ def export_graph(g, name):
 subax1 = plt.subplot()
 export_graph(dep, "dep")
 
-clusters_dict_parts = {}
-clusters_dict_conns = {}
+clusters_dict = {}
 final_con = process_graph(dep, con)
 print("FIN")
 export_graph(final_con, "res")
@@ -153,20 +150,14 @@ while final_con.order() > 0:
 
     layer += 1
 
-print(clusters_dict_parts)
-print(clusters_dict_conns)
+print(clusters_dict)
 print(layers_dict)
 
 with open("exports/export-layers.csv", 'w') as file:
     for lay in layers_dict.items():
         file.write(f"LAYER{lay[0]},{';'.join(lay[1])}\n")
 
-with open("exports/export-clusters-parts.csv", 'w') as file:
-    for lay in clusters_dict_parts.items():
+with open("exports/export-clusters.csv", 'w') as file:
+    for lay in clusters_dict.items():
         for clst in lay[1]:
-            file.write(f"LAYER{lay[0]},{clst[0]},{';'.join(clst[1])}\n")
-
-with open("exports/export-clusters-conns.csv", 'w') as file:
-    for lay in clusters_dict_conns.items():
-        for clst in lay[1]:
-            file.write(f"LAYER{lay[0]},{clst[0]},{';'.join(clst[1])}\n")
+            file.write(f"{lay[0]},{clst[0]},{';'.join(clst[1])},{';'.join(clst[2])},{';'.join(clst[3])}\n")
