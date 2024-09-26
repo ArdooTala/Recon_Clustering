9c39ef26a92b462bd4b1c8aaf9e17b91aea9f3b9
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-09-26 18:35:48 +0200

    Get export path from input

--- src/io_utils/file_writer.py
+++ src/io_utils/file_writer.py
@@ -6,13 +6,13 @@ import pathlib
 def export_graph_viz(g, name):
     # nx.nx_agraph.to_agraph(g).draw(f"exports/{name}.pdf", prog="neato",
     #                                args="-Gdiredgeconstraints=true -Gmode='ipsep' -Goverlap='prism5000'")
-    nx.nx_agraph.to_agraph(g).draw(f"../exports/{name}.pdf", prog="dot",
+    nx.nx_agraph.to_agraph(g).draw(name, prog="dot",
                                    args="-Grankdir='LR' -Granksep=1.5 -Gsplines='false'")
 
 
-def export_stages(stages_dict):
+def export_stages(stages_dict, name):
     print(stages_dict)
-    with open("../exports/export-components.csv", 'w') as file:
+    with open(name, 'w') as file:
         for stg in stages_dict.keys():
             for cmp in stages_dict[stg]:
                 comp = stages_dict[stg][cmp]
@@ -20,15 +20,15 @@ def export_stages(stages_dict):
                     f"{stg:02},{cmp:02},{';'.join(comp['conns'])},{';'.join(comp['parts'])},{';'.join(comp['added'])},{';'.join(comp['group'])}\n")
 
 
-def export_gantt(gantt_dict):
-    with open("../exports/export-stage_gantt.csv", 'w') as file:
+def export_gantt(gantt_dict, name):
+    with open(name, 'w') as file:
         for el in gantt_dict.items():
             file.write(
                 f"{el[0]},{el[1]['start']},{el[1]['end']},{el[1]['latest']},{';'.join(el[1]['deps'])},{';'.join(el[1]['children'])}\n")
 
 
-def export_clusters(clusters_dict):
-    with open("../exports/export-clusters.csv", 'w') as file:
+def export_clusters(clusters_dict, name):
+    with open(name, 'w') as file:
         for lay in clusters_dict.items():
             for clst in lay[1]:
                 file.write(f"{lay[0]},{clst[0]},{';'.join(clst[1])},{';'.join(clst[2])},{';'.join(clst[3])}\n")
@@ -89,4 +89,4 @@ def export_graph_to_inkscape(g, name):
             "inkscape:connection-end": f"#shape_{edge[1]}"
         })
 
-    tree.write(f"../exports/{name}.svg")
+    tree.write(name)
--- src/main.py
+++ src/main.py
@@ -2,37 +2,39 @@ from src.io_utils import graph_parser, file_writer
 from src.io_utils import graph_generator
 from src.graph_solver import graph_solver
 import networkx as nx
-
+from pathlib import Path
 
 if __name__ == "__main__":
+    export_path = Path("../exports/")
+    if not export_path.exists():
+        export_path.mkdir()
+
     con, dep = graph_generator.graph_from_gh_csv("../assemblies/ReconSlab_Top-Connectivity.csv")
-    file_writer.export_graph_viz(dep, "01-dep")
-    file_writer.export_graph_to_inkscape(dep, "01-dep")
+    file_writer.export_graph_viz(dep, export_path / "01-dep.pdf")
+    file_writer.export_graph_to_inkscape(dep, export_path / "01-dep.svg")
 
     final_ass = graph_solver.direct_cluster_sccs(con.copy(), dep.copy(), 0)
-    file_writer.export_graph_viz(final_ass, "02-res_dep")
-    file_writer.export_graph_to_inkscape(final_ass, "02-res_dep")
+    file_writer.export_graph_viz(final_ass, export_path / "02-res_dep.pdf")
+    file_writer.export_graph_to_inkscape(final_ass, export_path / "02-res_dep.svg")
 
     final_con = graph_solver.convert_ass_dep_to_con_dep(final_ass)
-    file_writer.export_graph_viz(final_con, "03-res_clustered")
-    file_writer.export_graph_to_inkscape(final_con, "03-res_clustered")
+    file_writer.export_graph_viz(final_con, export_path / "03-res_clustered.pdf")
+    file_writer.export_graph_to_inkscape(final_con, export_path / "03-res_clustered.svg")
 
     final_con = graph_solver.replace_cluster_with_conns(final_con)
-    file_writer.export_graph_viz(final_con, "04-res_expanded")
-    file_writer.export_graph_to_inkscape(final_con, "04-res_expanded")
+    file_writer.export_graph_viz(final_con, export_path / "04-res_expanded.pdf")
+    file_writer.export_graph_to_inkscape(final_con, export_path / "04-res_expanded.svg")
 
     stages_dict = graph_parser.generate_stages(con, final_con)
-    file_writer.export_stages(stages_dict)
+    file_writer.export_stages(stages_dict, export_path / "export-components.csv" )
 
     stages_graph = graph_solver.generate_stages_graph(final_con, stages_dict)
-    file_writer.export_graph_viz(stages_graph, "06-stages_dep")
+    file_writer.export_graph_viz(stages_graph, export_path / "06-stages_dep.pdf")
+    file_writer.export_graph_to_inkscape(final_con, export_path / "06-stages_dep.svg")
 
-    file_writer.export_clusters(graph_solver.clusters_dict)
+    file_writer.export_clusters(graph_solver.clusters_dict, export_path / "export-clusters.csv")
 
     gantt_dict = graph_parser.generate_gantt(final_con)
-    file_writer.export_gantt(gantt_dict)
-
-    nx.write_gexf(dep, f"../exports/01-dep.gexf")
-
+    file_writer.export_gantt(gantt_dict, export_path / "export-stage_gantt.csv")
 
     print("FIN")
