d2091faefaff74172527e3fb2465fed4b3e773ce
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-10-03 18:13:07 +0200

    Add new matplotlib viz

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from recongraph.io_utils import graph_parser, file_writer
+from recongraph.io_utils import graph_parser, file_writer, graph_visualizer
 from recongraph.graph_solver import graph_solver
 from pathlib import Path
 
@@ -12,18 +12,22 @@ if __name__ == "__main__":
     con = eg.con
     file_writer.export_graph_viz(con, export_path / "01-dep.pdf")
     file_writer.export_graph_to_inkscape(con, export_path / "01-dep.svg")
+    graph_visualizer.viz_dag(con)
 
     final_ass = graph_solver.direct_cluster_sccs(con.copy())
     file_writer.export_graph_viz(final_ass, export_path / "02-res_dep.pdf")
     file_writer.export_graph_to_inkscape(final_ass, export_path / "02-res_dep.svg")
+    graph_visualizer.viz_dag(final_ass)
 
     final_con = graph_solver.convert_ass_dep_to_con_dep(final_ass)
     file_writer.export_graph_viz(final_con, export_path / "03-res_clustered.pdf")
     file_writer.export_graph_to_inkscape(final_con, export_path / "03-res_clustered.svg")
+    graph_visualizer.viz_dag(final_con)
 
     final_con = graph_solver.replace_cluster_with_conns(final_con)
     file_writer.export_graph_viz(final_con, export_path / "04-res_expanded.pdf")
     file_writer.export_graph_to_inkscape(final_con, export_path / "04-res_expanded.svg")
+    graph_visualizer.viz_dag(final_con)
 
     stages_dict = graph_parser.generate_stages(con, final_con)
     # file_writer.export_stages(stages_dict, export_path / "export-components.csv" )
--- src/recongraph/io_utils/graph_visualizer.py
+++ src/recongraph/io_utils/graph_visualizer.py
@@ -4,5 +4,32 @@ import matplotlib.pyplot as plt
 
 def viz_g(g):
     subax1 = plt.subplot()
-    nx.draw_circular(g, with_labels=True, font_weight='bold')
+    pos = nx.spectral_layout(g)
+    nx.draw_networkx(g, pos=pos, with_labels=True, font_weight='bold')
+    plt.show()
+
+def viz_dag(g):
+    gg = g.copy()
+    dg = nx.subgraph_view(
+        g,
+        filter_edge=lambda e1, e2: g[e1][e2].get("EDGE_TYPE", None) != "COLL",
+    )
+    graph = dg.copy()
+    stage = 0
+    while graph.order() > 0:
+        print(graph)
+
+        sources = [x for x, ind in graph.in_degree if ind == 0]
+        print(f"Source Nodes: {sources}")
+        if not sources:
+            raise Exception("FUCK!...No Sources in Graph")
+        for src in sources:
+            gg.nodes[src]['subset'] = stage
+        graph.remove_nodes_from(sources)
+
+        stage += 1
+
+    subax1 = plt.subplot()
+    pos = nx.multipartite_layout(gg)
+    nx.draw_networkx(gg, pos=pos, with_labels=True, font_weight='bold')
     plt.show()
