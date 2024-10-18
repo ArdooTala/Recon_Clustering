9eaddfb2f21b38f8df77149a8a5d12b2a6d53172
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-10-18 14:33:23 +0200

    Code Cleanup

--- src/recongraph/processors/graph_parser.py
+++ src/recongraph/processors/graph_parser.py
@@ -62,63 +62,6 @@ def extract_stages(assembly, stages):
     return stages_dict
 
 
-def generate_stages(assembly, con_dep):
-    graph = con_dep.copy()
-    con = nx.subgraph_view(
-        assembly,
-        filter_edge=lambda e1, e2: assembly[e1][e2]["EDGE_TYPE"] != "COLL",
-    ).copy()
-    stages_dict = {}
-    all_parts = []
-
-    logger.info("Generating Stages")
-    stage = 0
-    while graph.order() > 0:
-        logger.debug(f"Stage: {stage}")
-        source_conn_nodes = [x for x, ind in graph.in_degree if ind == 0]
-        logger.debug(f"\t{len(source_conn_nodes)} Source Connection Nodes: {source_conn_nodes}")
-        if not source_conn_nodes:
-            raise Exception("FUCK!...No Sources in Graph")
-        assembly_sub_graph = _get_dep_graph_from_connections(con, source_conn_nodes)
-
-        # Export
-        stages_dict[stage] = {}
-        component_count = 0
-
-        components_graph = nx.subgraph_view(
-            con,
-            filter_node=lambda n: (con.nodes.data('stage', default=stage)[n] < stage) or (n in assembly_sub_graph)
-        )
-        connected_components = nx.weakly_connected_components(components_graph)
-        for connected_component in connected_components :
-            component = assembly_sub_graph.subgraph(connected_component)
-            if not component:
-                continue
-            logger.debug(f"\t\tComponent: {component} > {component.nodes}")
-            stages_dict[stage][component_count] = {}
-            comp_conns = [n for n in component if assembly_sub_graph.nodes[n]["TYPE"] == "CONN"]
-            comp_parts = [n for n in component if assembly_sub_graph.nodes[n]["TYPE"] == "PART"]
-            comp_added = [p for p in comp_parts if p not in all_parts]
-            all_parts += comp_added
-
-            for node in comp_added + comp_conns:
-                con.nodes[node]['stage'] = stage
-                con.nodes[node]['stage_component'] = component_count
-
-            stages_dict[stage][component_count]["conns"] = comp_conns
-            stages_dict[stage][component_count]["parts"] = comp_parts
-            stages_dict[stage][component_count]["added"] = comp_added
-            # stages_dict[stage][component_count]["group"] = comp_group
-            stages_dict[stage][component_count]["group"] = [c for c in  connected_component if
-                                                            con.nodes(data="TYPE")[c] == "PART"]
-
-            component_count += 1
-        graph.remove_nodes_from(source_conn_nodes)
-        stage += 1
-
-    return stages_dict
-
-
 def generate_gantt(graph):
     if not nx.is_directed_acyclic_graph(graph):
         raise Exception("Gantt cannot be generated because the graph is not a DAG")
