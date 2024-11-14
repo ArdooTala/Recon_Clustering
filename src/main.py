dd3bff1bd8ef3d9bdba4e34ddf7857d097057365
Ardeshir Talaei <ardeshir.talaei@gmail.com>
2024-11-14 19:25:52 +0100

    Add new Example Graph

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-from timeit import timeit
-from timeit import default_timer as timer
 import networkx as nx
 from recongraph.io_utils import file_writer, graph_generator
 from recongraph.processors import graph_parser, graph_solver
@@ -17,6 +15,7 @@ if __name__ == "__main__":
         file_writer.pygraphviz_export(graph, path.with_suffix(".pdf"))
 
         if not pos or not bbox:
+            pass
             # pos, bbox = graph_visualizer.multipartite_layout_by_connections(graph)
             pos, bbox = graph_visualizer.pygraphviz_layout(graph)
         file_writer.inkscape_export(graph, path.with_suffix(".svg"), pos, bbox)
@@ -25,7 +24,10 @@ if __name__ == "__main__":
     # Load Assembly
     # assembly = graph_generator.graph_from_gh_csv("../assemblies/ReconSlab_Top-Connectivity.csv")
     # assembly = graph_generator.graph_from_dot_file("../assemblies/simple.dot")
-    assembly = nx.read_gml("../assemblies/exception2.gml")
+    # assembly = nx.read_gml("../assemblies/exception2.gml")
+    # assembly = nx.read_gml("../assemblies/no_rdg.gml")
+    # assembly = graph_generator.graph_from_dot_file("../assemblies/simple.dot")
+    assembly = graph_generator.graph_from_dot_file("../assemblies/extended_scc.dot")
     # assembly = nx.read_gml("../assemblies/extended.gml")
 
     viz_and_save(assembly, export_path / "01-dep.pdf")
@@ -39,7 +41,8 @@ if __name__ == "__main__":
     # end = timer()
     # print(end - start)
     # start2 = timer()
-    # con_g = graph_parser._ass_2_con(resolved)
+    con_g = graph_parser._ass_2_con(resolved)
+    viz_and_save(con_g, export_path / "transitive_closure.pdf")
     # tg2 = list(nx.topological_generations(con_g))
     # end2 = timer()
     # print(end2 - start2)
@@ -55,8 +58,9 @@ if __name__ == "__main__":
     print(components)
 
     graph_visualizer.viz_g(graph_parser.get_component(resolved, 0, 0))
-    graph_visualizer.viz_g(graph_parser.get_component(resolved, 0, 1))
     graph_visualizer.viz_g(graph_parser.get_component(resolved, 1, 0))
+    graph_visualizer.viz_g(graph_parser.get_component(resolved, 1, 1))
+    graph_visualizer.viz_g(graph_parser.get_component(resolved, 2, 0))
     exit()
     stages_dict = graph_parser.extract_stages(assembly, stages)
     file_writer.export_stages(stages_dict, export_path / "export-early_components.csv")
